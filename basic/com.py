0711a2c60a12d455d0ff910e57c2bc8902c26017
irissun9602 <iris9602@naver.com>
2021-05-02 21:18:06 +0900

    Feat openpyxl module to create xlsx sheet

---
+++ basic/com.py
@@ -0,0 +1,12 @@
+from openpyxl import Workbook
+
+write_wb = Workbook()
+
+write_ws = write_wb.create_sheet('생성시트')
+
+write_ws = write_wb.active
+write_ws['A1'] = '숫자'
+
+write_ws.append([1,2,3,])
+
+write_wb.save('/Users/ms/PycharmProjects/pythonGrammer/basic/숫자.xlsx')
