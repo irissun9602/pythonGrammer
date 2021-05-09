e703bfd9af17b4f1d6a20bcbf640d97956bd7036
irissun9602 <iris9602@naver.com>
2021-05-09 19:23:18 +0900

    Create 4-3.py

---
+++ algorithm/4-3.py
@@ -0,0 +1,19 @@
+#현재 나이트의 위치 입력받기
+now_location = input()
+row = int(now_location[1]) - 1 #인덱스 0부터 시작하는 열
+column = int(ord(now_location[0])) - int(ord('a'))   #인덱스 0부터 시작하는 행
+
+print(row, column)
+steps = [(-2, -1), (-2, 1), (2, 1), (2, -1), (-1, 2), (-1, -2), (1, 2), (1, -2)]
+count = 0
+
+for step in steps:
+    x = step[0]+ row
+    y = step[1] +column
+    if (0<=x<=8) and (0<=y<=8):
+        print(step[0], step[1])
+        count += 1
+
+print(count)
+
+
