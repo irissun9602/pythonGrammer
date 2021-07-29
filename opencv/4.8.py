607e7c82b453d9f145216831ade39d38f9e0eb1c
irissun9602 <iris9602@naver.com>
2021-07-29 20:14:27 +0900

    Feat: Add y-coordinate position mark
    맨 위가 ypos=0, 맨 아래가 ypos=960

--- opencv/4.8.py
+++ opencv/4.8.py
@@ -11,12 +11,16 @@ height = 960
 img = np.zeros((height, width, 3), np.uint8)
 
 times = ypos = 0
+
+#y좌표가 960일때의 위치를 빨간 원으로 표시
+cv2.circle(img, (256, 960), 10, (0, 0, 255), -1)
+
 while True:
     if ypos+30 <height:
         cv2.circle(img, (256, 30+ypos), 10, (255, 0, 0), -1)
         times +=1
         ypos = int((9.8 * times**2)/2)
-        print(time, ':', ypos)
+        print(times, ':', ypos)
         time.sleep(1)
     cv2.imshow('Clock', img)
     if cv2.waitKey(100) >= 0:
