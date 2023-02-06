566b6153ae515d11b1bb81dd244b90e3d511ddb6
irissun9602 <iris9602@naver.com>
2023-02-06 21:58:08 +0900

    refactor: dfs, bfs algorithm

---
+++ algorithm/5-8.ice.py
@@ -0,0 +1,32 @@
+# N, m을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+
+# 2차원 리스트의 맵 정보 입력받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >=m :
+        return False
+    if graph[x][y] == 0:
+        #해당 노드 방문 처리
+        graph[x][y] = 1
+
+        # 상, 하, 좌, 우 재귀적 호출
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+result = 0
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
